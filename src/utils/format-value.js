073c7e0cbf3324966c65380459a41cbc0f5bae88
Timo Franz <timo@dumpark.com>
2018-08-10 13:43:12 +1200

    add documentation and improve formatting

--- src/utils/format-value.js
+++ src/utils/format-value.js
@@ -1,13 +1,33 @@
+/**
+  * Format number with thousand separator ','
+  * @param {number|string} x a number
+  * @return {string} a number with thousand separator
+  *
+  * @author [tmfrnz](https://github.com/tmfrnz)
+  */
 const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
+/**
+  * Format values according to type e.g. for display in axis and graph labels
+  *
+  * @param {number} value the number to formatValue
+  * @param {string} type the type on of: percentage, absolute
+  * @param {bool} [inThousands=false] if value should be shortened to thousands, appending 'k'
+  * @return {string} formatted number
+  *
+  * @author [tmfrnz](https://github.com/tmfrnz)
+  */
 export default function formatValue(value, type, inThousands = false) {
   switch (type) {
     case 'percentage' :
+      // percentage rounded to 1 decimal
       return `${Math.round(value * 10) / 10}%`;
     case 'absolute' :
       if (inThousands) {
-        return value === 0 ? value : `${value / 1000}k`;
+        // value in thousands
+        return value === 0 ? value : `${numberWithCommas(Math.round(value / 1000))}k`;
       }
+      // value with thousands separator
       return numberWithCommas(value);
     default:
       return value;
