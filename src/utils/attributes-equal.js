b540e37f6e8cf3ed1a0ee9547757c266d55d2963
Timo Franz <timo@dumpark.com>
2018-08-10 14:04:27 +1200

    add documentation and improve function

--- src/utils/attributes-equal.js
+++ src/utils/attributes-equal.js
@@ -1,10 +1,21 @@
-// comparison of attribute values, force string, check 'null' if unspecified
+/**
+  * Quasi-equality test of 2 values, allowing comparing numbers and strings
+  *
+  * @param {number|string} testValue the value to test
+  * @param {number|string} value the value to test against
+  * @return {boolean} if values are quasi equal
+  *
+  * @author [tmfrnz](https://github.com/tmfrnz)
+  */
 export default function attributesEqual(testValue, value) {
-  return typeof value !== 'undefined'
-    && value !== null
-    && value.toString() === (
-      (typeof testValue === 'undefined' || testValue === null)
-        ? 'null'
-        : testValue.toString()
-    );
+  if (typeof value === 'undefined' || typeof testValue === 'undefined') {
+    return undefined;
+  }
+  if (testValue === null) {
+    return value === null || value === 'null';
+  }
+  if (value === null) {
+    return testValue === null || testValue === 'null';
+  }
+  return value.toString() === testValue.toString();
 }
