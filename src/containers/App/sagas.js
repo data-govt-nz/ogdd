b6f04e5fb14d73bf0c805839ae6969609edc5b80
Timo Franz <timo@dumpark.com>
2018-08-23 18:01:13 +1200

    improve doc #16, refactor

--- src/containers/App/sagas.js
+++ src/containers/App/sagas.js
@@ -20,7 +20,7 @@ import { dataRequested, loadError, dataLoaded, navigationOccured } from './actio
 const MAX_LOAD_ATTEMPTS = 5;
 
 /**
- * Navigate Saga:
+ * Generator function. Navigate Saga:
  * - Composes to new route string for location
  * - Calls router
  * - Also makes sure page is scrolled to top on hash change
@@ -66,98 +66,109 @@ function* navigateSaga({ location, args }) {
   }
 }
 
-// Pass in our generator we want to restart, and some function
-// to handle potential errors thrown within said generator.
-// following https://codeburst.io/try-again-more-redux-saga-patterns-bfbc3ffcdc
-const autoRestart = (generator, handleError, maxTries = MAX_LOAD_ATTEMPTS) =>
-  function* autoRestarting(...args) {
-    let n = 0;
-    while (n < maxTries) {
-      n += 1;
-      try {
-        yield call(generator, ...args);
-        break;
-      } catch (err) {
-        if (n >= maxTries) {
-          yield handleError(err, ...args);
-        }
+/**
+ * Generator function. Function for restarting sagas multiple times before giving up and calling the error handler.
+ * - following https://codeburst.io/try-again-more-redux-saga-patterns-bfbc3ffcdc
+ *
+ * @param {function} generator the saga generator to be restarted
+ * @param {function} handleError the error handler after X unsuccessful tries
+ * @param {integer} maxTries the maximum number of tries
+ */
+const autoRestart = (generator, handleError, maxTries = MAX_LOAD_ATTEMPTS) => function* autoRestarting(...args) {
+  let n = 0;
+  while (n < maxTries) {
+    n += 1;
+    try {
+      yield call(generator, ...args);
+      break;
+    } catch (err) {
+      if (n >= maxTries) {
+        yield handleError(err, ...args);
       }
     }
-  };
+  }
+};
 
 
 /**
- * Load data saga:
+ * Generator function. Load data saga:
  * - Load data from API or files
  * - Dispatch load event with loaded data
+ * - Resets request time on error
  *
- * @param {object} payload key: data set key, value: data set definition
+ * @param {object} payload {key: data set key, value: data set definition}
  */
-const loadDataSaga = autoRestart(
-  function* loadData({ key, value }) {
-    // check if already loading
-    const requestedAt = yield select(selectRequestedAt, key);
-    if (!requestedAt) {
-      try {
-        // First record that we are requesting
-        yield put(dataRequested(key, Date.now()));
-        // load from JSON file
-        if (value.source === 'json') {
-          // fetch json file
-          const path = `${value.path}${value.filename}`;
-          const response = yield fetch(
-            window.global.OGDD_JS_PATH
-            ? `${window.global.OGDD_JS_PATH}${path}`
-            : path
-          );
-          const responseBody = yield response.json();
-          if (responseBody) {
-            yield put(dataLoaded(key, responseBody));
-          }
+function* loadDataSaga({ key, value }) {
+  // check if already loading
+  const requestedAt = yield select(selectRequestedAt, key);
+  // only load when not already loading
+  if (!requestedAt) {
+    try {
+      // first record that we are requesting (preventing to load again)
+      yield put(dataRequested(key, Date.now()));
+      // load from API
+      if (value.source === 'api') {
+        // fetch data from data.govt api (CKAN, see http://docs.ckan.org/en/latest/maintaining/datastore.html#api-reference)
+        // limit parameter defaults to 100, setting high number to practically turn limits off - after 10 years we should not have more than 1000 rows
+        const path = `${value.path}?resource_id=${value.resourceId}&limit=999999`;
+        const response = yield fetchJsonp(path);
+        const responseBody = yield response.json();
+        if (responseBody) {
+          yield put(dataLoaded(key, responseBody.result.records));
         }
-        // load from API
-        if (value.source === 'api') {
-          // fetch data from data.govt api (CKAN, see http://docs.ckan.org/en/latest/maintaining/datastore.html#api-reference)
-          // limit parameter defaults to 100, setting high number to practically turn limits off - after 10 years we should not have more than 1000 rows
-          const path = `${value.path}?resource_id=${value.resourceId}&limit=999999`;
-          const response = yield fetchJsonp(path);
-          const responseBody = yield response.json();
-          if (responseBody) {
-            yield put(dataLoaded(key, responseBody.result.records));
-          }
-        }
-        // load from CSV file
-        if (value.source === 'csv') {
-          const path = `${value.path}${value.filename}`;
-          const response = yield fetch(
-            window.global.OGDD_JS_PATH
-            ? `${window.global.OGDD_JS_PATH}${path}`
-            : path
-          );
-          const responseBody = yield response.text();
-          if (responseBody) {
-            yield put(dataLoaded(key, csvParse(responseBody)));
-          }
-        }
-      } catch (err) {
-        // // Clear the request time on error, and try again
-        yield put(dataRequested(key, null));
-        throw new Error(err);
       }
+      // load from JSON file
+      if (value.source === 'json') {
+        // fetch json file
+        const path = `${value.path}${value.filename}`;
+        const response = yield fetch(
+          window.global.OGDD_JS_PATH
+          ? `${window.global.OGDD_JS_PATH}${path}`
+          : path
+        );
+        const responseBody = yield response.json();
+        if (responseBody) {
+          yield put(dataLoaded(key, responseBody));
+        }
+      }
+      // load from CSV file
+      if (value.source === 'csv') {
+        const path = `${value.path}${value.filename}`;
+        const response = yield fetch(
+          window.global.OGDD_JS_PATH
+          ? `${window.global.OGDD_JS_PATH}${path}`
+          : path
+        );
+        const responseBody = yield response.text();
+        if (responseBody) {
+          yield put(dataLoaded(key, csvParse(responseBody)));
+        }
+      }
+    } catch (err) {
+      // clear the request time on error, and try again
+      yield put(dataRequested(key, null));
+      // throw error
+      throw new Error(err);
     }
-  },
-  function* handleError(err, { key }) {
-    // Whoops Save error after max attampets
-    yield put(loadError(key, err));
-  },
-  MAX_LOAD_ATTEMPTS,
-);
+  }
+}
+/**
+ * Generator function. Load data error handler:
+ * - Record load error
+ *
+ * @param {object} payload {key: data set key}
+ */
+function* loadDataErrorHandler(err, { key }) {
+  yield put(loadError(key, err));
+}
 
 /**
- * Root saga manages watcher lifecycle
+ * Generator function. Root saga manages watcher lifecycle for navigate and loadData sagas
+ * loadDataSaga is restarted several times before thorwing an error
+ *
  * @return {void}
  */
 export default function* rootSaga() {
   yield takeEvery(NAVIGATE, navigateSaga);
-  yield takeEvery(LOAD_DATA, loadDataSaga);
+  yield takeEvery(LOAD_DATA, autoRestart(loadDataSaga, loadDataErrorHandler, MAX_LOAD_ATTEMPTS));
 }
